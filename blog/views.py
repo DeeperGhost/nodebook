d07c37bf31994a988ff788c334881f69f7921ff2
KarlsonJuy <kareolis@gmail.com>
2019-04-02 17:11:22 +1000

    login add

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+# def post_list(request):
+#     return render(request, 'blog/post_list.html', {})
+
+def login(request):
+    return render(request, 'blog/login.html', {})
